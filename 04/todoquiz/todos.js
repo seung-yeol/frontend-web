0b4493e058a06f7c9194eeb31eb8c68f382c097e
seung-yeol <oks153123@naver.com>
2017-11-03 17:41:30 +0900

    todo

--- 04/todoquiz/todos.js
+++ 04/todoquiz/todos.js
@@ -3,34 +3,33 @@
 var work = document.getElementById("work");
 var list = document.getElementById("list");
 
-
 work.addEventListener('keydown', function (event){
   if (event.keyCode == 13) {
       console.log('과연');
       var str = work.value;
+      if (str === "") {
+        return;
+      }
+      else {
+        var listChild = document.createElement("li");
+        var deleteBtn = document.createElement("button");
+        num++;
 
-      var listChild = document.createElement("li");
-      var deleteBtn = document.createElement("button");
+        listChild.innerHTML =
+            "<input type='checkbox' class='toggle-checked'>"+
+            "<span class='text'>"+ str +"</span>";
+        listChild.appendChild(deleteBtn);
 
-      listChild.innerHTML =
-          "<input type='checkbox' class='toggle-checked'>"+
-          "<span class='text'>"+ str +"</span>";
-      listChild.appendChild(deleteBtn);
+        deleteBtn.classList.add("delete")
+        deleteBtn.innerHTML = "x";
+        deleteBtn.addEventListener("click", function(){
+          listChild.remove();
 
-      deleteBtn.style.float = "right";
-      deleteBtn.style.background = "none";
-      deleteBtn.style.position = "relative";
-      deleteBtn.style.border = "none";
-      deleteBtn.style.fontSize = "1em";
-      deleteBtn.style.fontWeight = "bold";
-      deleteBtn.innerHTML = "x";
-      deleteBtn.addEventListener("click", function(){
-        listChild.remove();
-      });
+        });
 
-      list.appendChild(listChild);
+        list.appendChild(listChild);
 
-      work.value = "";
+        work.value = "";
+      }
   }
-
 })
